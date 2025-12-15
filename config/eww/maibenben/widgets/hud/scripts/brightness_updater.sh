f90036690e1812b07f3482c332a0dc7e2df923b1
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-12-15 12:34:02 +0700

    updated

---
+++ config/eww/maibenben/widgets/hud/scripts/brightness_updater.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+eww="$HOME/.config/eww/widgets/hud"
+
+if [ -n "$1" ]; then
+  brightnessctl s ${1}% 
+fi
+
+BRIGHTNESS=$(brightnessctl -m | cut -d ',' -f4 | tr -d '%')
+
+if [ "$BRIGHTNESS" -le 10 ]; then
+  color="#1a4a8c"
+elif [ "$BRIGHTNESS" -le 20 ]; then
+  color="#2060a3"
+elif [ "$BRIGHTNESS" -le 30 ]; then
+  color="#2676ba"
+elif [ "$BRIGHTNESS" -le 40 ]; then
+  color="#2c8cd1"
+elif [ "$BRIGHTNESS" -le 50 ]; then
+  color="#32a2e8"
+elif [ "$BRIGHTNESS" -le 60 ]; then
+  color="#38b8ff"
+elif [ "$BRIGHTNESS" -le 70 ]; then
+  color="#5ec6ff"
+elif [ "$BRIGHTNESS" -le 80 ]; then
+  color="#84d4ff"
+elif [ "$BRIGHTNESS" -le 90 ]; then
+  color="#aae2ff"
+else
+  color="#d0f0ff"
+fi
+
+if [ "$BRIGHTNESS" -le 25 ]; then
+  icon="󰃜"
+elif [ "$BRIGHTNESS" -le 50 ]; then
+  icon="󰃝"
+elif [ "$BRIGHTNESS" -le 75 ]; then
+  icon="󰃞"
+else
+  icon="󰃠"
+fi
+
+eww update -c $eww br-icon=$icon
+eww update -c $eww br-color=$color
+echo "$BRIGHTNESS"
+
