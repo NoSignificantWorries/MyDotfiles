8a8a67a0d0b3ea4b148151d4634f25011dfdd10f
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-12-15 10:13:30 +0700

    updated

--- config/eww/maibenben/stash/apps/kill.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-config="$HOME/.config/eww/widgets/apps"
-
-eww close -c "$config" apps-window 2>/dev/null
-
--- config/eww/maibenben/stash/apps/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-config="$HOME/.config/eww/widgets/apps"
-
-eww daemon 2>/dev/null
-
-eww --force-wayland open-many -c "$config" apps-window 2>/dev/null
