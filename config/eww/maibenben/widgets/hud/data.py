5e2f67374254a3636b1082cece3e87ae6ac3ed46
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-12-29 18:13:43 +0700

    eww updated

--- config/eww/maibenben/widgets/hud/data.py
+++ config/eww/maibenben/widgets/hud/data.py
@@ -1,12 +1,57 @@
 import os
 import asyncio
 import subprocess
+from pathlib import Path
 
+
+CONFIG = Path("~/.config/eww/widgets/hud").expanduser()
+
+VOL_ICONS = ["", "", "", ""]
+VOL_COLORS = [
+    "#6c7086",
+    "#005f56",
+    "#007f66",
+    "#009f76",
+    "#00bf86",
+    "#00df96",
+    "#00ffa6",
+    "#40ffb6",
+    "#80ffc6",
+    "#b0ffd6",
+    "#e0ffe6"
+]
+BR_ICONS = ["󰃜", "󰃝", "󰃞", "󰃠"]
+BR_COLORS = [
+    "#1a4a8c",
+    "#2060a3",
+    "#2676ba",
+    "#2c8cd1",
+    "#32a2e8",
+    "#38b8ff",
+    "#5ec6ff",
+    "#84d4ff",
+    "#aae2ff",
+    "#d0f0ff"
+]
+BAT_ICONS_CHARGING = ["󰢟", "󰢜", "󰂆", "󰂇", "󰂈", "󰢝", "󰂉", "󰢞", "󰂋", "󰂅"]
+BAT_ICONS_NORMAL = ["󱃍", "󰁺", "󰁻", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰁹"]
+BAT_COLORS = [
+    "#ed8796",
+    "#ea909e",
+    "#e799a6",
+    "#e3a3af",
+    "#e0acb8",
+    "#d0b7a2",
+    "#c0c28c",
+    "#b1cd76",
+    "#a1d860",
+    "#91e34a"
+]
 
 class EwwUpdater:
     @staticmethod
     def update(var: str, value: str):
-        subprocess.run(['eww', 'update', f'{var}={value}'], stdout=subprocess.DEVNULL)
+        subprocess.run(['eww', '-c', CONFIG, 'update', f'{var}={value}'], stdout=subprocess.DEVNULL)
 
 
 class CmdRunner:
@@ -19,22 +64,77 @@ class CmdRunner:
                 stderr=asyncio.subprocess.PIPE,
             )
             stdout, _ = await proc.communicate()
-            return stdout.strip()
+            try:
+                text = stdout.decode('utf-8').strip()
+            except UnicodeDecodeError:
+                text =  stdout.decode('latin1').strip()
+            return text
         except:
             return "N/A"
 
 
-async def base_sys_loop():
+async def bat_loop():
     while True:
-        vol, br, bat = await asyncio.gather(
+        bat = await CmdRunner.get_output("acpi -b")
+
+        bat = bat.split(", ")
+        status = "Charging" in bat[0]
+        percent = int(bat[1][:-1])
+        time = bat[-1].split(" ")[0].split(":")
+        H, M = time[0], time[1]
+        if status:
+            time = f"{H}h {M}m to full"
+        else:
+            time = f"{H}h {M}m to empty"
+        idx = round(percent / 100 * 9)
+        bat_color = BAT_COLORS[idx]
+        bat_icon = BAT_ICONS_CHARGING[idx] if status else BAT_ICONS_NORMAL[idx]
+        EwwUpdater.update("bat-status", f"{bat_icon} {percent}%")
+        EwwUpdater.update("bat-color", bat_color)
+        EwwUpdater.update("bat-time", time)
+
+        await asyncio.sleep(30)
+
+
+async def vol_br_loop():
+    last_muted = None
+    last_vol = None
+    last_br = None
+    while True:
+        vol, br = await asyncio.gather(
             CmdRunner.get_output("wpctl get-volume @DEFAULT_AUDIO_SINK@"),
             CmdRunner.get_output("brightnessctl -m"),
-            CmdRunner.get_output("acpi -b"),
         )
-        print(f"Громкость: {vol}")
-        print(f"Br: {br}")
-        print(f"Bat: {bat}")
-        await asyncio.sleep(2)
+        muted = "[MUTED]" in vol
+        vol = vol.split(" ")
+        vol = round(float(vol[1]) * 100)
+        if last_vol != vol or last_muted != last_muted:
+            if muted:
+                vol_color = VOL_COLORS[0]
+                vol_icon = VOL_ICONS[0]
+            else:
+                vol_color = VOL_COLORS[round(vol / 100 * 9 + 1)]
+                vol_icon = VOL_ICONS[round(vol / 100 * 2 + 1)]
+            EwwUpdater.update("vol-mute", "true" if muted else "false")
+            EwwUpdater.update("vol-icon", vol_icon)
+            EwwUpdater.update("vol-color", vol_color)
+            EwwUpdater.update("vol", str(vol))
+
+            last_muted = muted
+            last_vol = vol
+
+        br = br.split(",")
+        br = int(br[3][:-1])
+        if last_br != br:
+            br_color = BR_COLORS[round(br / 100 * 9)]
+            br_icon = BR_ICONS[round(br / 100 * 3)]
+            EwwUpdater.update("br", str(br))
+            EwwUpdater.update("br-color", br_color)
+            EwwUpdater.update("br-icon", br_icon)
+
+            last_br = br
+
+        await asyncio.sleep(1)
 
 
 async def hyprland_events():
@@ -63,7 +163,8 @@ async def hyprland_events():
 
 async def main():
         tasks = [asyncio.create_task(hyprland_events()),
-                 asyncio.create_task(base_sys_loop())]
+                 asyncio.create_task(vol_br_loop()),
+                 asyncio.create_task(bat_loop())]
         await asyncio.gather(*tasks)
 
 
