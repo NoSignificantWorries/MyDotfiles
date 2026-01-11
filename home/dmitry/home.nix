671cad7dce8e6f559427cda67e2ae0a81bd9c610
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2026-01-11 17:32:22 +0700

    hyprland enabled

--- home/dmitry/home.nix
+++ home/dmitry/home.nix
@@ -5,6 +5,16 @@
   home.homeDirectory = "/home/dmitry";
   home.stateVersion = "25.11";
 
-  home.packages = [];
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemdIntegration = true;
+  };
+
+  home.packages = [
+    xdg-desktop-portal-hyprland
+    hyprpaper
+    hypridle
+    wl-clipboard
+  ];
 }
 
--- hosts/nixos-pc/configuration.nix
+++ hosts/nixos-pc/configuration.nix
@@ -11,8 +11,7 @@
   boot.loader.systemd-boot.configurationLimit = 3;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos-pc"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos-pc";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -48,6 +47,15 @@
     options = "--delete-older-than 7d";
   };
 
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dmitry = {
     isNormalUser = true;
@@ -56,11 +64,13 @@
     packages = with pkgs; [];
   };
 
+  hardware.opengl = {
+    enable = true;
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim
     wget
@@ -68,31 +78,9 @@
     git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 
 }
