4a05647ec3ce7432ac2ce1246b9f693e7a07dbe1
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2026-01-11 16:55:40 +0700

    added flakes.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-mahager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos-pc = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./hosts/nixos-pc/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixos-pc = import ./home/dmitry/home.nix;
+        }
+      ];
+    };
+  };
+}
+
