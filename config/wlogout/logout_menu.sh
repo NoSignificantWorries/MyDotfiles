5135058b38fc11de0afc8cb67acfdf9e6b371884
NoSignificantWorries <dmitry.a.karpachev@gmail.com>
2026-01-30 16:35:05 +0700

    removed logout from wlogout and swaync

--- config/wlogout/logout_menu.sh
+++ config/wlogout/logout_menu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 if ! pgrep -x "wlogout" > /dev/null; then
-    wlogout --layout ~/.config/wlogout/layout --css ~/.config/wlogout/style.css -b 6
+    wlogout --layout ~/.config/wlogout/layout --css ~/.config/wlogout/style.css -b 5
 fi
 
